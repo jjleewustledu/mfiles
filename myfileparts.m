5c45794c34aea49aced8c44942025c98dd4e6f36
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-03-09 01:53:35 -0600

    supports floating point numbers in filenames

--- myfileparts.m
+++ myfileparts.m
@@ -1,5 +1,6 @@
 function [pth,prefix,ext] = myfileparts(fstr)
 %% MYFILEPARTS extends fileparts to regard myfileparts.SUFFIXES as single file extensions.
+%  It protects trailing floating point numbers such as "/path/to/file_tagged_1.23".
 %  @param fstr is char or cell; cell with length == 1 is interpreted as char.
 %  @return pth.
 %  @return prefix.
@@ -19,13 +20,30 @@ if (iscell(fstr))
     end
     fstr = fstr{1};
 end
+
+% base cases
+
+% recognized SUFFIXES
 for s = 1:length(SUFFIXES)
     if (lstrfind(fstr, SUFFIXES{s}))
         [pth,prefix,ext] = filepartsx(fstr, SUFFIXES{s});
         return
     end
 end
+
+% trivial
 [pth,prefix,ext] = fileparts(fstr);
+
+% check for floating-point at end
+pe = [prefix ext];
+r = regexp(pe, '\d+\.\d+', 'match');
+if (~isempty(r)) % found f.p.
+    idx_f = strfind(pe, r{end}) + length(r{end}) - 1; % end idx of f.p.
+    if (idx_f > length(prefix)) % f.p. extends into ext
+        prefix = pe;
+        ext = '';
+    end
+end
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
 % ===== EOF ====== [/Users/jjlee/Local/src/mlcvl/mfiles/myfileparts.m] ======  

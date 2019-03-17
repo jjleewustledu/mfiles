6c0962b57743656556d3d4556436b99fa7f9045d
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-03-17 15:50:34 -0500

    Logger → ILogger.  s~=0 → error.  Relocated mlbash{Diary,Logger} errors to the respective functions.

--- mlbashDiary.m
+++ mlbashDiary.m
@@ -1,9 +1,8 @@
 function [s,r] = mlbashDiary(cmdline, diaryfn)
-%% MLBASHDIARY
-%% Version $Revision$ was created $Date$ by $Author$,  
-%% last modified $LastChangedDate$ and checked into svn repository $URL$ 
-%% Developed on Matlab 9.0.0.307022 (R2016a) Prerelease 
-%% $Id$ 
+    %% MLBASHDIARY
+    %  Version $Revision$ was created 2016 by jjlee,  
+    %  last modified 20190313 160638 and checked into git repository MATLAB-Drive/mfiles.
+    %  Developed on Matlab 9.0.0.307022 (R2016a) Prerelease 
 
     ip = inputParser;
     addRequired( ip, 'cmdline', @ischar);
@@ -11,11 +10,10 @@ function [s,r] = mlbashDiary(cmdline, diaryfn)
     parse(ip, cmdline, diaryfn);
 
     diary(diaryfn);
-    [s,r] = system(cmdline);
-    fprintf('mlbashDiary.cmdline:\n%s\n', cmdline);
-    fprintf('mlbashDiary.s:  %i\n', s);
-    fprintf('mlbashDiary.r:\n%s\n', r);
-    diary('off');
+    fprintf('mlbashDiary.cmdline:  %s\n', cmdline);
+    if (s ~= 0)
+        error('mfiles:ChildProcessError', 'mlbashDiary:\n%s\nreturned s->%i\n%s', cmdline, s, r);
+    end
 end
 
 
--- mlbashLogger.m
+++ mlbashLogger.m
@@ -1,19 +1,21 @@
 function [s,r] = mlbashLogger(cmdline, logger)
-%% MLBASHLOGGER
-%% Version $Revision$ was created $Date$ by $Author$,  
-%% last modified $LastChangedDate$ and checked into svn repository $URL$ 
-%% Developed on Matlab 9.0.0.307022 (R2016a) Prerelease 
-%% $Id$ 
+    %% MLBASHLOGGER
+    %  Version $Revision$ was created 2016 by jjlee,
+    %  last modified 20190313 160902 and checked into git repository MATLAB-Drive/mfiles.
+    %  Developed on Matlab 9.0.0.307022 (R2016a) Prerelease 
 
     ip = inputParser;
     addRequired( ip, 'cmdline', @ischar);
-    addRequired( ip, 'logger', @(x) isa(x, 'mlpipeline.Logger'));
+    addRequired( ip, 'logger', @(x) isa(x, 'mlpipeline.ILogger'));
     parse(ip, cmdline, logger);
 
     [s,r] = system(cmdline);
-    logger.add(sprintf('mlbashLogger.cmdline:\n%s\n', cmdline));
+    logger.add(sprintf('mlbashLogger.cmdline:  %s\n', cmdline));
     logger.add(sprintf('mlbashLogger.s:  %i\n', s));
     logger.add(sprintf('mlbashLogger.r:\n%s\n', r));
+    if (s ~= 0)
+        error('mfiles:ChildProcessError', 'mlbashLogger:\n%s\nreturned s->%i\n%s', cmdline, s, r);
+    end
 end
 
 

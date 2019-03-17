82ab2d94482e0e889ffa0af4c0914cb826bd33f9
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-03-17 15:51:41 -0500

    stub for testing

---
+++ mcrContext.m
@@ -0,0 +1,13 @@
+function mcrContext()
+%% MCRCONTEXT ... 
+%% Version $Revision$ was created $Date$ by $Author$,  
+%% last modified $LastChangedDate$ and checked into repository $URL$,  
+%% developed on Matlab 9.5.0.1049112 (R2018b) Update 3.  Copyright 2019 John Joowon Lee. 
+
+fprintf('inside mcrContext')
+
+
+
+
+% Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
+% ===== EOF ====== [/Users/jjlee/MATLAB-Drive/mfiles/mcrContext.m] ======  

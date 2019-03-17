1d0eaa7f9b38fdba1627a8c2ad4627f5a59bf84b
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-03-17 15:46:12 -0500

    '.' in fileprefixes may disrupt some 4dfp tools.

--- mydatetimestr.m
+++ mydatetimestr.m
@@ -1,10 +1,33 @@
-function s = mydatetimestr(dt)
-%% MYDATETIMESTR specifies datestr with a preferred format
-%% Version $Revision$ was created $Date$ by $Author$,  
-%% last modified $LastChangedDate$ and checked into repository $URL$,  
-%% developed on Matlab 9.5.0.1049112 (R2018b) Update 3.  Copyright 2019 John Joowon Lee. 
+function s = mydatetimestr(dt, varargin)
+%% MYDATETIMESTR specifies datestr with a preferred format.
+%  @param required dt is compatible with datestr.
+%  @param subsec is logical, specifying whether to include millisec and microsec; default := false.
+%  @param fullstop is char array that does not conflict with 
+%         https://www.mathworks.com/help/releases/R2018b/matlab/ref/datestr.html#btenptl-1-formatOut ;
+%         otherwise fullstop := '_'.
+%  @return char, e.g., 'DT20190317123902.000000'.
 
-s = sprintf('DT%s000', datestr(dt, 'yyyymmddHHMMSS.FFF'));
+%  Version $Revision$ was created 2019 by jjlee,  
+%  last modified 20190317 and checked into repository MATLAB-Drive/mfiles,  
+%  developed on Matlab 9.5.0.1049112 (R2018b) Update 3.  Copyright 2019 John Joowon Lee. 
+
+ip = inputParser;
+addRequired( ip, 'dt', @(x) ~isempty(x));
+addParameter(ip, 'subsec', false, @islogical);
+addParameter(ip, 'fullstop', '.', @ischar);
+parse(ip, dt, varargin{:});
+fullstop = ip.Results.fullstop;
+subsec = ip.Results.subsec;
+
+if (contains(fullstop, ...
+    {'yyyy' 'yy' 'QQ' 'mmmm' 'mmm' 'mm' 'm' 'dddd' 'ddd' 'dd' 'd' 'HH' 'MM' 'SS' 'FFF' 'AM' 'PM'}))
+    fullstop = '_';
+end
+if (~isempty(fullstop) && subsec)
+    s = sprintf('DT%s000', datestr(dt, sprintf('yyyymmddHHMMSS%sFFF', fullstop)));
+else
+    s = sprintf('DT%s', datestr(dt, 'yyyymmddHHMMSS'));
+end
 
 
 

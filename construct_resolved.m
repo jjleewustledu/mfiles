56e6399e45ad873a1d09e85bd3d22a3f4047f1d6
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-03-17 15:57:39 -0500

    Tidied docs.   Allowing ac←[] to enable tracer value to specify ac.   Using optional parameters and enabling char values to tidy use of MCR, Docker and Singularity.   Simplifying call to TracerDirector2.constructResolved.

--- construct_resolved.m
+++ construct_resolved.m
@@ -1,45 +1,36 @@
 function those = construct_resolved(varargin)
-    %% CONSTRUCT_RESOLVED supports t4_resolve for niftypet.  It is the top level of a Matlab Compiler library project.
-    %  Usage:  resolve_pet('sessionExpr', sessExpr, 'tracer', aTrac, 'ac', tf) 
+    %% CONSTRUCT_RESOLVED supports t4_resolve for niftypet.  It is the top level of a Matlab Compiler Runtime project.
+    %  Usage:  construct_resolved([projectsExpr, sessionsExpr, tracerExpr, ac])
+    %  e.g.:   >> construct_resolved('CCIR_00123', 'ses-E0012*', 'OO_DT20190101*-Converted-NAC*')
     %  
-    %  @precondition ct 4dfp in fullfile(getenv('SUBJECTS_DIR'), sessExpr); 
-    %  listmode .bf/.dcm and umap folder of .dcm in 
-    %  tracerLocation := fullfile(getenv('SUBJECTS_DIR'), sessExpr, vExpr, tracerFolder), e.g.,
-    %  tracerFolder := 'FDG_V1-NAC' | 'FDG_V1-AC'.
+    %  @precondition fullfile(subjectsDir, project, session, 'umapSynth_op_T1001_b43.4dfp.*') and
+    %                         subjectsDir := getenv('PPG_SUBJECTS_DIR')
+    %  @precondition files{.bf,.dcm} in fullfile(subjectsDir, project, session, 'LM', '')
+    %  @precondition files{.bf,.dcm} in fullfile(subjectsDir, project, session, 'norm', '')
+    %  @precondition FreeSurfer recon-all results in fullfile(subjectsDir, project, session, 'mri', '')
     %
-    %  @param sessionsExpr is char, e.g., 'ses-E00026'.
-    %  @param tracer       is char, e.g., 'FDG'.
-    %  @param ac, attentuaion corection, is logical.  Default := false.
-    %  @return new filesystem tree at tracerLocation containing:  
-    %  if not ac:  
-    %      zoomed tracer 4dfp, epoch folders, logs, t4s, unzoomed umaps resolved to 
-    %      no-attenuation-correction frame reconstructions;
-    %  else:
-    %      zoomed tracer 4dfp resolved to last frame of attenuation/scatter/randoms corrected reconstructions.
-    %
-    %
-    %
-    %  @param  named sessionsExp is char, specifying session directories to match by DirTool.
-    %  @param  named visitsExp   is char, specifying visit   directories to match by DirTool.
-    %  @param  named scanList    is numeric := trace scan indices.
-    %  @param  named tracer      is char    and passed to SessionData.
-    %  @param  named ac          is logical and passed to SessionData.
-    %  @return those             is a cell-array of objects specified by factoryMethod.
-    %  @return dtsess            is an mlsystem.DirTool for sessions.
+    %  @param projectsExpr is char, e.g., 'CCIR_00123' or globbed.
+    %  @param sessionsExpr is char, e.g., 'ses-E01234' or globbed.
+    %  @param tracerExpr   is char, e.g., 'FDG_DT20000101090000.000000-Converted-NAC' or globbed.
+    %  @param ac           is logical or []; default := [].  
+    %         Set to logical to constrain globbing attentuaion corection to '-NAC' or '-AC'.
+    %  @return results in fullfile(subjectsDir, project, session, tracer) 
+    %          for elements of projectsExpr, sessionsExpr and tracerExpr.
+    %  @return cell array of objects specified by mlraichle.TracerDirector2.constructResolved().
     
-    %TRACERS = {'OC' 'HO' 'OO' 'FDG'}; 
-    AC = false;
+    TRACERS = {'OC*' 'HO*' 'OO*' 'FDG*'}; 
         
     import mlsystem.* mlraichle.*; %#ok<NSTIMP>
+    import mlpet.DirToolTracer;
     ip = inputParser;
     ip.KeepUnmatched = true;
-    addParameter(ip, 'projectsExpr', 'CCIR_*', @ischar);
-    addParameter(ip, 'sessionsExpr', 'ses-*', @ischar);
-    addParameter(ip, 'tracer', 'HO*', @(x) ischar(x) || iscell(x));
-    addParameter(ip, 'ac', AC);
+    addOptional( ip, 'projectsExpr', 'CCIR_*', @ischar);
+    addOptional( ip, 'sessionsExpr', 'ses-*', @ischar);
+    addOptional( ip, 'tracer', TRACERS, @(x) ischar(x) || iscell(x));
+    addOptional( ip, 'ac', []);
     addParameter(ip, 'frameAlignMethod', '', @ischar); % align_10243
     addParameter(ip, 'compAlignMethod', '', @ischar); % align_multiSpectral
-    addParameter(ip, 'fractionalImageFrameThresh', [], @isnumeric);
+    addParameter(ip, 'fractionalImageFrameThresh', [], @(x) isnumeric(x) || ischar(x));
     parse(ip, varargin{:});
     ipr = adjustParameters(ip.Results);
     projExpr = ipr.projectsExpr;
@@ -51,7 +42,7 @@ function those = construct_resolved(varargin)
         dtsess = DirTools(fullfile(dtproj.fqdns{iproj}, sessExpr));
         for isess = 1:length(dtsess.fqdns)
             pwd0 = pushd(dtsess.fqdns{isess});
-            dttrac = mlpet.DirToolTracer('tracer', ipr.tracer, 'ac', ipr.ac);
+            dttrac = DirToolTracer('tracer', ipr.tracer, 'ac', ipr.ac);
             for itrac = 1:length(dttrac.fqdns)
                 try
                     sessd = constructSessionData(ipr, dtproj.dns{iproj}, dtsess.dns{isess}, dttrac.dns{itrac});
@@ -59,8 +50,9 @@ function those = construct_resolved(varargin)
                     fprintf('construct_resolved:\n');
                     fprintf([evalc('disp(sessd)') '\n']);
                     fprintf(['\tsessd.TracerLocation->' sessd.tracerLocation '\n']);
+                    
                     warning('off', 'MATLAB:subsassigndimmismatch');
-                    those{isess,itrac} = TracerDirector2.constructResolved('sessionData', sessd, varargin{:});  %#ok<AGROW>
+                    those{isess,itrac} = TracerDirector2.constructResolved('sessionData', sessd);  %#ok<AGROW>
                     warning('on',  'MATLAB:subsassigndimmismatch');
                 catch ME
                     dispwarning(ME)
@@ -75,11 +67,17 @@ function those = construct_resolved(varargin)
 
     function ipr = adjustParameters(ipr)
         assert(isstruct(ipr));
-        results = {'projectsExpr' 'sessionsExpr'};
+        results = {'projectsExpr' 'sessionsExpr' 'tracer'};
         for r = 1:length(results)
             if (~lstrfind(ipr.(results{r}), '*'))
                 ipr.(results{r}) = [ipr.(results{r}) '*'];
             end
+        end
+        if (ischar(ipr.ac))
+            ipr.ac = strcmpi(ipr.ac, 'true');
+        end
+        if (ischar(ipr.fractionalImageFrameThresh))
+            ipr.fractionalImageFrameThresh = str2double(ipr.fractionalImageFrameThresh);            
         end
     end
     function sessd = constructSessionData(ipr, projf, sessf, tracf)

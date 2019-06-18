89fe6853c33261b50abbad324299e856527fd9c8
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-06-18 03:29:32 -0500

    → preferredTimeZone

--- setPreferredTimeZone.m
+++ setPreferredTimeZone.m
@@ -1,12 +1,12 @@
 function dt_ = setPreferredTimeZone(dt_)
-%% SETPREFERREDTIMEZONE ... 
-%  Usage:  adatetime = setPreferredTimeZone(adatetime) 
-%          ^ TimeZone := mlnipet.Resources.PREFERRED_TIMEZONE
+%% SETPREFERREDTIMEZONE
+%  @param dt_ is datetime
+%  @return dt_ is datetime with adjusted TimeZone
 %% Version $Revision$ was created $Date$ by $Author$,  
 %% last modified $LastChangedDate$ and checked into repository $URL$,  
 %% developed on Matlab 9.3.0.713579 (R2017b).  Copyright 2018 John Joowon Lee. 
 
-dt_.TimeZone = mlnipet.Resources.PREFERRED_TIMEZONE;
+dt_.TimeZone = mlpipeline.ResourcesRegistry.instance().preferredTimeZone;
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
 % ===== EOF ====== [/Users/jjlee/MATLAB-Drive/mfiles/setPreferredTimeZone.m] ======  

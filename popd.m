49eb6ce93c4419be7f8d4032b0a5ce3c3ae7b94b
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-07-01 16:25:10 -0500

    tidying

--- popd.m
+++ popd.m
@@ -1,26 +1,16 @@
-function [pwdLast,pwd0] = popd(pwd0)
-%% POPD ... 
-%  Usage:  [locationLast,location0] = popd(location0) 
-%           ^            ^                 ^ isdir(...)
+function pwd1 = popd(pwd0)
+%% POPD 
+%  @param pwd0 is the pwd before call to pushd.
+%  @return pwd1 is the pwd after call to pushd.
+
 %% Version $Revision$ was created $Date$ by $Author$,  
 %% last modified $LastChangedDate$ and checked into svn repository $URL$ 
 %% Developed on Matlab 9.1.0.441655 (R2016b) 
 %% $Id$ 
 
-pwdLast = pwd;
-assert(isdir(pwd0));
-try
-    cd(pwd0);
-catch ME
-    handerror(ME);
-end
-
-
-
-
-
-
-
+pwd1 = pwd;
+assert(isfolder(pwd0));
+cd(pwd0);
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
 % ===== EOF ====== [/Users/jjlee/Local/src/mlcvl/mfiles/popd.m] ======  
--- pushd.m
+++ pushd.m
@@ -1,32 +1,19 @@
-function pwd0 = pushd(pth)
+function pwd0 = pushd(pwd1)
 %% PUSHD ... 
-%  Usage:  location0 = pushd(location) 
-%          ^                 ^ isdir(...)
+%  @param pwd1 is the pwd after calling pushd.
+%  @return pwd0 is the pwd before calling pushd.
+
 %% Version $Revision$ was created $Date$ by $Author$,  
 %% last modified $LastChangedDate$ and checked into svn repository $URL$ 
 %% Developed on Matlab 9.1.0.441655 (R2016b) 
 %% $Id$ 
 
-if (isempty(pth))
+if isempty(pwd1) || ~isfolder(pwd1)
     pwd0 = pwd;
     return
 end
-
-assert(isdir(pth));
 pwd0 = pwd;
-try
-    cd(pth);
-catch ME
-    handerror(ME);
-end
-
-
-
-
-
-
-
-
+cd(pwd1);
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
 % ===== EOF ====== [/Users/jjlee/Local/src/mlcvl/mfiles/pushd.m] ======  

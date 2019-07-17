d58b7e112323ddf9de3257c997d5f2c65b056f01
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-07-17 00:01:07 -0500

    useful for idioms "for obj = glob(glob_string); do_something(obj{1}); end"

---
+++ ascol.m
@@ -0,0 +1,18 @@
+function [x,tf] = ascol(x)
+%% ASCOL 
+%% Version $Revision$ was created $Date$ by $Author$,  
+%% last modified $LastChangedDate$ and checked into svn repository $URL$ 
+%% Developed on Matlab 9.1.0.441655 (R2016b) 
+%% $Id$ 
+
+[x,tf] = mlsystem.VectorTools.ensureColVector(x);
+
+
+
+
+
+
+
+
+% Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
+% ===== EOF ====== [/Users/jjlee/Local/src/mlcvl/mfiles/ensureColVector.m] ======  

dcbd44d8bae4a0901bed4256b6b9c01e2e2bd1d6
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-07-16 23:59:49 -0500

    useful open source

---
+++ lns.m
@@ -0,0 +1,11 @@
+function lns(varargin)
+%% LNS
+%  Version $Revision$ was created $Date$ by $Author$,  
+%  last modified $LastChangedDate$ and checked into svn repository $URL$ 
+%  Developed on Matlab 9.1.0.441655 (R2016b) 
+%  $Id$ 
+
+mlfourdfp.FourdfpVisitor.lns(varargin{:});
+
+% Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
+

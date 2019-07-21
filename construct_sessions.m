5021867eba210ed052f53189ff11b61dbcb7a1f3
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-07-21 16:35:54 -0500

    add aufbau parameters that launch preparatory behaviors from mlraichle.SubjectData

--- construct_sessions.m
+++ construct_sessions.m
@@ -1,5 +1,11 @@
 function construct_sessions(subFolder)
 %% CONSTRUCT_SESSIONS ... 
+function construct_sessions(subFolder, varargin)
+%% CONSTRUCT_SESSIONS 
+%  @param required subFolder is a folder.
+%  @param aufbauSubjectsDir is logical (default false); it initiates SubjectData.aufbauSubjectsDir and immediately returns.
+%  @param aufbauSessionPath is logical (default false); it initiates SubjectData.aufbauSessionPath and immediately returns.
+
 %% Version $Revision$ was created $Date$ by $Author$,  
 %% last modified $LastChangedDate$ and checked into repository $URL$,  
 %% developed on Matlab 9.5.0.1067069 (R2018b) Update 4.  Copyright 2019 John Joowon Lee. 
@@ -9,6 +15,27 @@ function construct_sessions(subFolder)
     setenv('PROJECTS_DIR', '/scratch/jjlee/Singularity')
     setenv('SUBJECTS_DIR', '/scratch/jjlee/Singularity/subjects')
     subPath = fullfile(getenv('SUBJECTS_DIR'), subFolder, '');
+    
+    ip = inputParser;
+    addParameter(ip, 'aufbauSubjectsDir', false, @islogical);
+    addParameter(ip, 'aufbauSessionPath', false, @islogical);
+    addParameter(ip, 'experimentPattern', '_E', @ischar)
+    addParameter(ip, 'sessionPattern', 'ses-*', @ischar)
+    addParameter(ip, 'tracerPattern', 'FDG_DT*.000000-Converted-AC', @ischar)
+    parse(ip, varargin{:})
+    
+    if ip.Results.aufbauSubjectsDir
+        subjectData = mlraichle.SubjectData();
+        subjectData.aufbauSubjectsDir();
+        return
+    end
+    if ip.Results.aufbauSessionPath
+        subjectData = mlraichle.SubjectData('subjectFolder', subFolder);
+        pth = fullfile(getenv('SUBJECTS_DIR'), subFolder);
+        subID = subFolder2subID(subjectData, subFolder);
+        subjectData.aufbauSessionPath(pth, subjectData.subjectsJson.(subID), 'experimentPattern', ip.Results.experimentPattern);
+        return
+    end
     
     pwd0 = pushd(subPath);
     dt = DirTool('ses-*');

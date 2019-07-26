12b97fda09b9c47c88cd61179eea29279e4674fb
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-07-25 20:48:14 -0500

    nearly working, but t4 composition has a bug at subject-level

--- compose_t4s.m
+++ compose_t4s.m
@@ -1,10 +1,30 @@
 function sub_struct = compose_t4s()
-
-    % build sub_struct
+    %% COMPOSE_T4S
+    
+    % build sub_struct := {
+    %   "tra_struct" = { 
+    %     "fdg" = { "fdgdt<datetime>_to_op_fdg_avgr1_t4" }, 
+    %     "ho"  = { "hodt<datetime>_to_op_fdg_avgr_t4", ... }, 
+    %     "oc"  = { "ocdt<datetime>_to_op_fdg_avgr1_t4", ... }, ...
+    %   },
+    %   "ses_struct" = {
+    %     "ses-E00001" = {
+    %       "tra_struct" = {
+    %         "fdg" = { "fdg_avgr1_to_op_fdg_avgr1_t4" }, 
+    %         "ho"  = { "hodt<datetime>_to_op_fdg_avgr_t4", ... }, 
+    %         "oc"  = { "ocdt<datetime>_to_op_fdg_avgr1_t4", ... }, ...
+    %   },
+    %     "ses-E00002" = {
+    %       "tra_struct" = {
+    %         "fdg" = { "fdgdt<datetime>_to_op_fdg_avgr1_t4" }, 
+    %         "ho"  = { "hodt<datetime>_to_op_fdg_avgr_t4", ... }, 
+    %         "oc"  = { "oc_avgr1_to_op_oc_avgr1_t4" }, ...
+    %     }
+    %   }
+    % }
    
     load('t4_obj.mat', 't4_obj')
     sub_struct = struct('tra_struct', t4_obj);
-
     for ses = asrow(glob('ses-E*'))
         try
             load(fullfile(ses{1}, 't4_obj.mat'), 't4_obj')
@@ -15,17 +35,16 @@ function sub_struct = compose_t4s()
         end
     end
 
-    % do t4_mul
+    % compose t4s:  tracer -> session -> subject -> T1001
 
     fv = mlfourdfp.FourdfpVisitor;
     ensuredir('resampling_restricted')
-
     tracers = {'fdg' 'ho' 'oo' 'oc'};
     for tra = tracers
         for itra = 1:length(sub_struct.tra_struct.(tra{1}))
             t4sub = sub_struct.tra_struct.(tra{1}){itra};
-            [sesfold,t4ses] = find_sesfold_and_t4(t4sub, sub_struct, tra{1});
-            fv.t4_mul(fullfile(sesfold, t4ses), t4sub, t4_ses2sub(t4sub))
+            [sesfold,t4ses] = find_sesfold_and_t4ses(t4sub, sub_struct, tra{1});
+            fv.t4_mul(t4sub, fullfile(sesfold, t4ses), t4_ses2sub(t4sub))
             fv.t4_mul(t4_ses2sub(t4sub), 'fdg_avgr1_to_T1001r1_t4', ...
                       fullfile('resampling_restricted', [tracerdt(t4sub) '_to_T1001_t4']))
         end   
@@ -33,16 +52,59 @@ function sub_struct = compose_t4s()
 end
 
 
-
-function [sesfold,t4] = find_sesfold_and_t4(sub_t4, sub_struct, tra)
+function [sesfold,t4] = find_sesfold_and_t4ses(sub_t4, sub_struct, tra)
+    %% @return sesfolder and t4 filename
+    for ses = asrow(fields(sub_struct.ses_struct))       
+        assert(~isempty(sub_struct.ses_struct.(ses{1}).tra_struct.(tra)), ...
+            'mfiles:RuntimeError', 'compose_t4s.find_sesfold_and_t4ses() received empty tracer list')
+        for tdt = asrow(sub_struct.ses_struct.(ses{1}).tra_struct.(tra))
+            if issingleton(sub_struct.ses_struct.(ses{1}).tra_struct.(tra)) && ...
+                    lstrfind(tracerpref(sub_t4), singletondt(tra, ses{1})) % found matching
+                sesfold = strrep(ses{1}, '_', '-');
+                t4 = sub_struct.ses_struct.(ses{1}).tra_struct.(tra){1}; % t4 ~ 'tracer_avgr1_to_op_tracer_avgr1_t4'
+                return
+            end
+            if strcmp(tracerpref(sub_t4), tracerpref(tdt{1})) % found matching
+                sesfold = strrep(ses{1}, '_', '-');
+                t4 = tdt{1};
+                return
+            end
+        end
+    end
+end
+function [sesfold,t4] = find_sesfold_and_t4ses_(sub_t4, sub_struct, tra)
+    %% @return sesfolder and t4 filename
+    for ses = asrow(fields(sub_struct.ses_struct))       
+        assert(~isempty(sub_struct.ses_struct.(ses{1}).tra_struct.(tra)), ...
+            'mfiles:RuntimeError', 'compose_t4s.find_sesfold_and_t4ses() received empty tracer list')
+        if issingleton(sub_struct.ses_struct.(ses{1}).tra_struct.(tra))
+            % special case for singleton sub_struct.ses_struct.(ses{1}).tra_struct.(tra)
+            
+            if lstrfind(tracerpref(sub_t4), singletondt(tra, ses{1})) % found matching
+                sesfold = strrep(ses{1}, '_', '-');
+                t4 = sub_struct.ses_struct.(ses{1}).tra_struct.(tra){1}; % t4 ~ 'tracer_avgr1_to_op_tracer_avgr1_t4'
+                return
+            end
+        else
+            for tdt = asrow(sub_struct.ses_struct.(ses{1}).tra_struct.(tra))
+                if strcmp(tracerpref(sub_t4), tracerpref(tdt{1})) % found matching
+                    sesfold = strrep(ses{1}, '_', '-');
+                    t4 = tdt{1};
+                    return
+                end
+            end
+        end
+    end
+end
+function [sesfold,t4] = find_sesfold_and_t4ses__(sub_t4, sub_struct, tra)
     for ses = asrow(fields(sub_struct.ses_struct))
         for tdt = asrow(sub_struct.ses_struct.(ses{1}).tra_struct.(tra)) % special for fdg
-            if strcmpi(tra, 'fdg') && lstrfind(tracerdt(sub_t4), fdgdt(ses{1}))
+            if strcmpi(tra, 'fdg') && lstrfind(tracerpref(sub_t4), fdgdt(ses{1}))
                 sesfold = strrep(ses{1}, '_', '-');
                 t4 = sub_struct.ses_struct.(ses{1}).tra_struct.fdg{1};
                 return
             end
-            if strcmp(tracerdt(sub_t4), tracerdt(tdt{1}))
+            if strcmp(tracerpref(sub_t4), tracerpref(tdt{1}))
                 sesfold = strrep(ses{1}, '_', '-');
                 t4 = tdt{1};
                 return
@@ -51,6 +113,7 @@ function [sesfold,t4] = find_sesfold_and_t4(sub_t4, sub_struct, tra)
     end
 end
 function tdt = fdgdt(ses1)
+    %% @returns first fdgdt12345678 object found in ses1
     sesfold = strrep(ses1, '_', '-');
     for g = glob(fullfile(sesfold, 'fdgdt*.4dfp.hdr'))
         tdt = mybasename(g{1});
@@ -60,7 +123,35 @@ function tdt = fdgdt(ses1)
     end
     error('mfiles:RuntimeError', 'compose_t4s.fdgdt.sesfold is missing fdg')
 end
+function tf = issingleton(tra_list)
+    tf = length(tra_list) == 1;
+end
+function tdt = singletondt(tra, ses1)
+    %% @returns first tracerdt12345678 object found in ses1
+    sesfold = strrep(ses1, '_', '-');
+    for g = glob(fullfile(sesfold, [tra 'dt*.4dfp.hdr']))
+        tdt = mybasename(g{1});
+        if ~isempty(regexp(tdt, sprintf('^%sdt\\d+$', tra), 'once'))
+            return
+        end
+    end
+    error('mfiles:RuntimeError', 'compose_t4s.fdgdt.sesfold is missing fdg')
+end
 function tdt = tracerdt(t4)
+    %% @returns tracerdt12345678 from some tracerdt12345678_to<...>_t4
+    t4fp = tracerpref(t4);
+    if regexp(t4fp, '^[a-z]+dt\d+$', 'once')
+        tdt = t4fp;
+        return
+    end
+    
+    % find associated datetime
+    g = glob([t4fp 'dt*.4dfp.*']);
+    tdt = strsplit(g{1}, '.');
+    tdt = tdt{1};
+end
+function tdt = tracerpref(t4)
+    %% @returns tracerdt12345678 from some tracerdt12345678_to<...>_t4
     tdt = strsplit(t4, '_');
     tdt = tdt{1};
 end
--- construct_resolved.m
+++ construct_resolved.m
@@ -1,8 +1,8 @@
-function those = construct_resolved(varargin)
+function construct_resolved(varargin)
     %% CONSTRUCT_RESOLVED supports t4_resolve for niftypet.  It is the top level of a Matlab Compiler Runtime project.
-    %  Usage:  construct_resolved([projectsExpr, sessionsExpr, tracerExpr, ac])
-    %  e.g.:   >> construct_resolved('CCIR_00123', 'ses-E0012*', 'OO_DT20190101*-Converted-NAC*')
-    %          >> construct_resolved('CCIR_00123/ses-E0012*/OO_DT20190101*-Converted-NAC*')
+    %  Usage:  construct_resolved(<folders experssion>)
+    %          e.g.:  >> construct_resolved('CCIR_00123/ses-E00123/OO_DT20190101.000000-Converted-NAC')    
+    %          e.g.:  >> construct_resolved('CCIR_00123/ses-E0012*/OO_DT*-Converted-NAC')
     %  
     %  @precondition fullfile(projectsDir, project, session, 'umapSynth_op_T1001_b43.4dfp.*') and
     %                         projectsDir := getenv('PROJECTS_DIR')
@@ -10,115 +10,38 @@ function those = construct_resolved(varargin)
     %  @precondition files{.bf,.dcm} in fullfile(projectsDir, project, session, 'norm', '')
     %  @precondition FreeSurfer recon-all results in fullfile(projectsDir, project, session, 'mri', '')
     %
-    %  @param projectsExpr is char, e.g., 'CCIR_00123' or globbed;
-    %         it may also contain two filesep instances, prompting an interpretation such that
-    %         projectsExpr == projectsExpr/sessionsExpr/tracerExp; sessionsExpr and tracerExpr will by consequently 
-    %         ignored.
-    %  @param sessionsExpr is char, e.g., 'ses-E01234' or globbed.
-    %  @param tracersExpr is char, e.g., 'FDG_DT20000101090000.000000-Converted-NAC' or globbed.
-    %  @param ac is logical or []; default := [].  
-    %         Set to logical to constrain globbing attentuaion corection to '-NAC' or '-AC'.
+    %  @param foldersExpr is char.
     %  @return results in fullfile(projectsDir, project, session, tracer) 
     %          for elements of projectsExpr, sessionsExpr and tracerExpr.
-    %  @return cell array of objects specified by mlraichle.TracerDirector2.constructResolved().
     %
     %  N.B.:  Setting environment vars PROJECTS_DIR or SUBJECTS_DIR is not compatible with many Docker or Singularity
-    %         use cases.
-    
-    TRACERS = {'OC*' 'HO*' 'OO*' 'FDG*'}; 
-        
-    import mlsystem.* mlraichle.*; %#ok<NSTIMP>
-    import mlpet.DirToolTracer;
-    ip = inputParser;
-    ip.KeepUnmatched = true;
-    addOptional( ip, 'projectsExpr', 'CCIR_*', @ischar);
-    addOptional( ip, 'sessionsExpr', 'ses-*', @ischar);
-    addOptional( ip, 'tracersExpr', TRACERS, @(x) ischar(x) || iscell(x));
-    addParameter(ip, 'ac', []);
-    addParameter(ip, 'ignoreFinishMark', true, @islogical);
-    addParameter(ip, 'frameAlignMethod', '', @ischar); % align_10243
-    addParameter(ip, 'compAlignMethod', '', @ischar); % align_multiSpectral
-    addParameter(ip, 'fractionalImageFrameThresh', [], @(x) isnumeric(x) || ischar(x));
-    addParameter(ip, 'reconstructLastEpoch', false, @islogical)
-    parse(ip, varargin{:});
-    ipr = adjustParameters(ip.Results);
-    projExpr = ipr.projectsExpr;
-    sessExpr = ipr.sessionsExpr;
-    those = {};
-    
-    dtproj = DirTools(fullfile(StudyRegistry.instance.projectsDir, projExpr));
-    for iproj = 1:length(dtproj.fqdns)
-        dtsess = DirTools(fullfile(dtproj.fqdns{iproj}, sessExpr));
-        for isess = 1:length(dtsess.fqdns)
-            pwd0 = pushd(dtsess.fqdns{isess});
-            dttrac = DirToolTracer('tracer', ipr.tracersExpr, 'ac', ipr.ac);
-            for itrac = 1:length(dttrac.fqdns)
-                try
-                    sessd = constructSessionData(ipr, dtproj.dns{iproj}, dtsess.dns{isess}, dttrac.dns{itrac});
-                    
-                    fprintf('construct_resolved:\n');
-                    fprintf([evalc('disp(sessd)') '\n']);
-                    fprintf(['\tsessd.TracerLocation->' sessd.tracerLocation '\n']);
-                    
-                    warning('off', 'MATLAB:subsassigndimmismatch');
-                    those{isess,itrac} = TracerDirector2.constructResolved( ...
-                        'sessionData', sessd, ...
-                        'reconstructLastEpoch', ipr.reconstructLastEpoch);  %#ok<AGROW>
-                    warning('on',  'MATLAB:subsassigndimmismatch');
-                catch ME
-                    dispwarning(ME)
-                    getReport(ME)
-                end
-            end
-            popd(pwd0);
-        end
-    end
-    
-    %% INTERNAL FUNC
+    %         use cases.    
 
-    function ipr = adjustParameters(ipr)
-        assert(isstruct(ipr));
-        if lstrfind(ipr.projectsExpr, filesep)
-            ss = strsplit(ipr.projectsExpr, filesep);
-            assert(3 == length(ss));
-            ipr.projectsExpr = ss{1};
-            ipr.sessionsExpr = ss{2};
-            ipr.tracersExpr = ss{3};
-        end
-        results = {'projectsExpr' 'sessionsExpr' 'tracersExpr'};
-        for r = 1:length(results)
-            if (~lstrfind(ipr.(results{r}), '*'))
-                ipr.(results{r}) = [ipr.(results{r}) '*'];
+    ss = strsplit(varargin{1}, filesep);
+    switch ss{1}
+        case {'CCIR_00754' 'CCIR_00559'}
+            mlraichle.TracerDirector2.constructResolvedStudy(varargin{:})
+            
+        case 'CCIR_00993'
+            mlan.TracerDirector2.constructResolvedStudy(varargin{:})
+            
+        case 'subjects'
+            if length(ss) > 1 && lstrfind(ss{2}, 'sub')
+                if length(ss) > 2 && lstrfind(ss{3}, 'ses')
+                    mlraichle.TracerDirector2.constructSessionsStudy(varargin{:})
+                    return
+                end
+                mlraichle.TracerDirector2.constructSubjectsStudy(varargin{:})
+                return
             end
-        end
-        if ischar(ipr.ac)
-            ipr.ac = strcmpi(ipr.ac, 'true');
-        end
-        if ischar(ipr.fractionalImageFrameThresh)
-            ipr.fractionalImageFrameThresh = str2double(ipr.fractionalImageFrameThresh);            
-        end
-    end
-    function sessd = constructSessionData(ipr, projf, sessf, scanf)
-        import mlraichle.*;
-        sessd = SessionData( ...
-            'studyData', StudyRegistry.instance, ...
-            'projectData', ProjectData('sessionStr', sessf), ...
-            'subjectData', SubjectData(), ...
-            'sessionFolder', sessf, ...
-            'scanFolder', scanf);
-        if (~isempty(ipr.fractionalImageFrameThresh))
-            sessd.fractionalImageFrameThresh = ipr.fractionalImageFrameThresh;
-        end
-        if (~isempty(ipr.frameAlignMethod))
-            sessd.frameAlignMethod = ipr.frameAlignMethod;
-        end
-        if (~isempty(ipr.compAlignMethod))
-            sessd.compAlignMethod = ipr.compAlignMethod;
-        end
-        if (~isempty(ipr.ignoreFinishMark))
-            sessd.ignoreFinishMark = ipr.ignoreFinishMark;
-        end
-    end    
+            warning('mfiles:RuntimeWarning', 'construct_resolved:  nothing to do')
+            
+        case 'subjects_00993'            
+            mlan.TracerDirector2.constructSessionsStudy(varargin{:})
+            
+        otherwise
+            error('mfiles:RuntimeError', 'construct_resolved.ss{1}->%s', ss{1})
+    end  
 end
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 

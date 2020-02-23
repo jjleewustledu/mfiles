716ce91962fbceb975b4eb4934aeb312fd611978
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-02-23 14:43:44 -0600

    added additional stats for methods *{p,P}hantom*()

---
+++ construct_phantom_stats.m
@@ -0,0 +1,23 @@
+function construct_phantom_stats()
+%% CONSTRUCT_PHANTOM_STATS walks the top-level folder containing CCIR_00* folders, then generates stats for all 
+%  enclosd phantom scans of calibration materials.
+%
+%% Version $Revision$ was created $Date$ by $Author$,  
+%% last modified $LastChangedDate$ and checked into repository $URL$,  
+%% developed on Matlab 9.7.0.1296695 (R2019b) Update 4.  Copyright 2020 John Joowon Lee. 
+
+cd('/data/nil-bluearc/raichle/PPGdata/jjlee/Singularity')
+for proj = globFoldersT('CCIR_00*')
+    pwd0 = pushd(proj{1});
+    for ses = globFoldersT('ses-E*')
+        pwd1 = pushd(ses{1});
+        fdgs = globFoldersT('FDG_DT*-Converted-*');
+        loc = fullfile(proj{1}, ses{1}, fdgs{end});
+        construct_phantom(loc, 'getstats', true)        
+        popd(pwd1)
+    end
+    popd(pwd0)
+end
+
+% Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
+% ===== EOF ====== [/Users/jjlee/MATLAB-Drive/mfiles/construct_phantom_stats.m] ======  

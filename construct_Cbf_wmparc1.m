5453b2ff3c24aeae3e4b01f8979ebcd46811f18d
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2020-09-15 19:12:17 -0500

    working for construct_Cbf_wmparc1()

--- construct_Cbf_wmparc1.m
+++ construct_Cbf_wmparc1.m
@@ -1,5 +1,5 @@
-function construct_Ks_wmparc1(subjectsExpr, Nthreads)
-%% e.g. construct_Ks_wmparc1('sub-S3*', 16)
+function construct_Cbf_wmparc1(subjectsExpr, Nthreads)
+%% e.g. construct_Cbf_wmparc1('sub-S58163*', 16)
 
 import mlraichle.*
 import mlraichle.AerobicGlycolysisKit.*
@@ -10,7 +10,7 @@ if ischar(Nthreads)
 end
 
 registry = MatlabRegistry.instance(); %#ok<NASGU>
-subjectsDir = '/data/nil-bluearc/raichle/PPGdata/jjlee/Singularity/subjects';
+subjectsDir = fullfile(getenv('SINGULARITY_HOME'), 'subjects');
 setenv('SUBJECTS_DIR', subjectsDir)
 setenv('PROJECTS_DIR', fileparts(subjectsDir))
 pwd1 = pushd(subjectsDir);
@@ -22,42 +22,45 @@ for sub = subjects
     subd = mlraichle.SubjectData('subjectFolder', sub{1});
     sesfs = subd.subFolder2sesFolders(sub{1});
 
-    for ses = sesfs
+    ses = {sesfs{3}};
+%    for ses = sesfs
         try
             sesd = SessionData( ...
                 'studyData', StudyData(), ...
                 'projectData', ProjectData('sessionStr', ses{1}), ...
                 'subjectData', subd, ...
                 'sessionFolder', ses{1}, ...
-                'tracer', 'FDG', ...
+                'tracer', 'HO', ...
                 'ac', true, ...
                 'region', 'wmparc1');            
             if sesd.datetime < mlraichle.StudyRegistry.instance.earliestCalibrationDatetime
                 continue
             end
-            fdgfn = sesd.fdgOnAtlas();
-            if ~isfile([myfileprefix(fdgfn) '_b43.mat'])
-                fdg = mlfourd.ImagingContext2(fdgfn);
-                fdg = fdg.blurred(4.3);
-                DispersedAerobicGlycolysisKit.ic2mat(fdg)
+            hofn = sesd.hoOnAtlas();
+            if ~isfile(hofn)
+                sesd.jitOn222(hofn)
+            end
+            if ~isfile([myfileprefix(hofn) '_b43.mat'])
+                ho = mlfourd.ImagingContext2(hofn);
+                ho = ho.blurred(4.3);
+                DispersedAerobicGlycolysisKit.ic2mat(ho)
             end
             filesys(idx).sesd = sesd; %#ok<AGROW>
             idx = idx + 1;
         catch ME
             handexcept(ME)
         end
-    end
+%    end
     popd(pwd0)
 end
 
-parfor (p = 1:length(filesys), Nthreads)    
+for p = 1:length(filesys) % (p = 1:length(filesys), Nthreads)    
     try
-        DispersedAerobicGlycolysisKit.constructKsByRegion(filesys(p).sesd); % memory ~ 5.5 GB
+        DispersedAerobicGlycolysisKit.constructFsByRegion(filesys(p).sesd); % memory ~ 5.5 GB
         
-        [cmrglc,Ks,msk] = DispersedAerobicGlycolysisKit.constructCmrglc(filesys(p).sesd);
-        Ksc = DispersedAerobicGlycolysisKit.iccrop(Ks, 1:4);
-        DispersedAerobicGlycolysisKit.ic2mat(Ksc)
-        DispersedAerobicGlycolysisKit.ic2mat(cmrglc)        
+        [cbf,msk] = DispersedAerobicGlycolysisKit.constructCbf(filesys(p).sesd);
+        Fsc = DispersedAerobicGlycolysisKit.iccrop(cbf, 1:3);
+        DispersedAerobicGlycolysisKit.ic2mat(Fsc)
         DispersedAerobicGlycolysisKit.ic2mat(msk)
     catch ME
         handwarning(ME)

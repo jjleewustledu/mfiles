5d84d9bf15397ee8a2544c101a1acb5860d0559b
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2021-03-18 11:22:42 -0500

    for stand-alone MCR & Docker

---
+++ DispersedAerobicGlycolysisKit_construct.m
@@ -0,0 +1,18 @@
+function DispersedAerobicGlycolysisKit_construct(varargin)
+%% DISPERSEDAEROBICGLYCOLYSISKIT_CONSTRUCT ... 
+%  e.g.:  construct('cbv', 'subjectsExpr', 'sub-S58163*', 'Nthreads', 1, 'region', 'wholebrain')
+%  e.g.:  construct('cbv', 'debug', true)
+%  @param required physiolog is char, e.g., cbv, cbf, cmro2, cmrglc.
+%  @param subjectsExpr is char, e.g., 'sub-S58163*'.
+%  @param region is char, e.g., wholebrain, wmparc1.
+%  @param debug is logical.
+%  @param Nthreads is numeric|char.
+%          
+%% Version $Revision$ was created $Date$ by $Author$,  
+%% last modified $LastChangedDate$ and checked into repository $URL$,  
+%% developed on Matlab 9.9.0.1592791 (R2020b) Update 5.  Copyright 2021 John Joowon Lee. 
+
+mlraichle.DispersedAerobicGlycolysisKit.construct(varargin{:})
+
+% Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
+% ===== EOF ====== [/Users/jjlee/MATLAB-Drive/mfiles/DispersedAerobicGlycolysisKit_construct.m] ======  

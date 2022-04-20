c81d103b4f4a9607edeeec6220e2ff6fc9e4358a
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2022-04-20 17:35:37 -0500

    adjusted exception handling

--- mlbash.m
+++ mlbash.m
@@ -19,26 +19,37 @@ function [s,r] = mlbash(cmdline, varargin)
     addParameter(ip, 'logger', [],        @(x) isa(x, 'mlpipeline.ILogger'));
     addParameter(ip, 'echo', false,       @islogical);     
     parse(ip, cmdline, varargin{:});    
-    s = 0; r = ''; %#ok<NASGU>
+    s = 0; r = '';    
     
-    if ~isempty(ip.Results.diaryFilename)
-        [s,r] = mlbashDiary(cmdline, ip.Results.diaryFilename);
-        return
+    try        
+        if ~isempty(ip.Results.diaryFilename)
+            [s,r] = mlbashDiary(cmdline, ip.Results.diaryFilename);
+            return
+        end
+        if ~isempty(ip.Results.logger)
+            [s,r] = mlbashLogger(cmdline, ip.Results.logger);
+            return
+        end    
+        if ip.Results.echo || mlpipeline.PipelineRegistry.instance.verbose
+            [s,r] = system(cmdline, '-echo');
+        else
+            [s,r] = system(cmdline);
+        end
+        
+        if s ~= 0
+            fprintf('====================== mlbash failed ======================\n');
+            fprintf('cmdline: %s\n', cmdline);
+            fprintf('s->%i; r->%s\n', s, r);
+        end
+    catch ME
+        fprintf('====================== mlbash exception ======================\n');
+        disp(ME);
+        disp(struct2str(ME.stack));
     end
-    if ~isempty(ip.Results.logger)
-        [s,r] = mlbashLogger(cmdline, ip.Results.logger);
-        return
-    end
-    
-    if ip.Results.echo || mlpipeline.PipelineRegistry.instance.verbose
-        [s,r] = system(cmdline, '-echo');
-    else
-        [s,r] = system(cmdline);
-    end
-    if s ~= 0
-        throw(MException('mfiles:ChildProcessError', ...
-            'mlbash:\n%s\nreturned status -> %i\n         result -> %s', cmdline, s, r));
-    end
+    %if s ~= 0
+    %    throw(MException('mfiles:ChildProcessError', ...
+    %        'mlbash:\n%s\nreturned status -> %i\n         result -> %s', cmdline, s, r));
+    %end
 end
 
 % Created with NEWFCN.m by Frank Gonzalez-Morphy (frank.gonzalez-morphy@mathworks.de) 
